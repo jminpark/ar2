9b0053baaed7320b3c0e83bc5a9c5e70007570d5
박정민/책임/DXT센터 서비스플랫폼담당 ThinQ Platform TP(jmin.park@lge.com) <jmin.park@lge.com>
2020-10-26 15:41:02 +0900

    ar icon test

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(window).on("load",function(){
+
+    //mCustomScrollbar
+    $(".container-wrap").mCustomScrollbar({
+        axis:"y",
+        theme:"minimal-dark",
+        scrollbarPosition:"inside",
+        mouseWheel:{ preventDefault: true }
+    });
+
+});
